a53a9e4dd092b21196b0275212ff11ac65450194
DevInfinix <singhsamarjeet2007@gmail.com>
2024-10-03 02:11:47 +0530

    sm8350-common: Switch to clang-r487747c Beta

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -114,6 +114,7 @@ TARGET_KERNEL_CONFIG := vendor/lahaina-qgki_defconfig
 TARGET_KERNEL_NO_GCC := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
     HOSTCFLAGS="-Wno-unused-command-line-argument"
+TARGET_KERNEL_CLANG_VERSION := r487747c
 
 # Include openssl headers
 CFLAGS += -I/usr/include/openssl
