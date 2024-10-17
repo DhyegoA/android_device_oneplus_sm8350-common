2f30f31bbcaf70f388ce8255bd7307aa268cf6cc
DevInfinix <singhsamarjeet2007@gmail.com>
2024-10-17 20:24:35 +0530

    sm8350-common: Welcome Android 15 with Pixelage!

--- common.mk
+++ common.mk
@@ -108,22 +108,11 @@ PRODUCT_PACKAGES += \
 # Blur
 TARGET_ENABLE_BLUR := true
 
-# Additional FOSS Applications
-PRODUCT_PACKAGES += \
-    ViMusic \
-    Droid-ify \
-    RevampedFMRadio
-
 # Camera
 $(call inherit-product-if-exists, vendor/oplus/camera/opluscamera.mk)
 
 # ViPER4AndroidFX
 $(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
-
-# Additional FOSS Applications
-# PRODUCT_PACKAGES += \
-#     ViMusic \
-#     Droid-ify
 
 PRODUCT_PACKAGES += \
     android.frameworks.stats@1.0.vendor \
